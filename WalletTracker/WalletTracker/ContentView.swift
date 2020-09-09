485f66daa18268c01257f1dc8539dbda018dfe4c
William Ching <willykamus@gmail.com>
2020-09-08 20:37:32 -0400

    no message

---
+++ WalletTracker/WalletTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WalletTracker
+//
+//  Created by William Ching on 2020-09-08.
+//  Copyright © 2020 William Ching. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
